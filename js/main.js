a45eed18516425d9349773694afa7b8ec4c70a49
Maria Midkiff <maria.midkiff@icloud.com>
2023-08-13 06:27:57 -0700

    restructure adding audio file

--- js/main.js
+++ js/main.js
@@ -1,36 +1,61 @@
+// original used with audio files not in a folder
+// document.getElementById('purple').onclick = function () {
+	// playSound();
+	// partyPurple();
+// };
+// document.getElementById('green').onclick = function () {
+	// playSoundGreen();
+	// partyGreen();
+// };
+// document.getElementById('blue').onclick = function () {
+	// playSoundBlue();
+	// partyBlue();
+// };
+// document.getElementById('orange').onclick = function () {
+	// playSoundOrange();
+	// partyOrange();
+// };
+
+// function playSound() {
+	// const audio = new Audio('C:100Dev Classes\\class12-materials\\class12-materials\\background-picker - Maria Midkiff\\cow-moos-76219.mp3');
+	// audio.play();
+// }
+// function playSoundGreen() {
+	// const audio = new Audio('C:100Dev Classes\\class12-materials\\class12-materials\\background-picker - Maria Midkiff\\cat-meow.wav');
+	// audio.play();
+// }
+// function playSoundBlue() {
+	// const audio = new Audio('C:100Dev Classes\\class12-materials\\class12-materials\\background-picker - Maria Midkiff\\canadian-geese.wav');
+	// audio.play();
+// }
+// function playSoundOrange() {
+	// const audio = new Audio('C:100Dev Classes\\class12-materials\\class12-materials\\background-picker - Maria Midkiff\\funk-trumpet_87bpm_G_minor.wav');
+	// audio.play();
+// }
+	
+// updated so sound can be heard on git hub
 document.getElementById('purple').onclick = function () {
-	playSound();
+	playSound('audio/cow-moos-76219.mp3');
 	partyPurple();
 };
 document.getElementById('green').onclick = function () {
-	playSoundGreen();
+	playSound('audio/cat-meow.wav');
 	partyGreen();
 };
 document.getElementById('blue').onclick = function () {
-	playSoundBlue();
+	playSound('audio/canadian-geese.wav');
 	partyBlue();
 };
 document.getElementById('orange').onclick = function () {
-	playSoundOrange();
+	playSound('audio/funk-trumpet_87bpm_G_minor.wav');
 	partyOrange();
 };
 
-function playSound() {
-	const audio = new Audio('C:100Dev Classes\\class12-materials\\class12-materials\\background-picker - Maria Midkiff\\cow-moos-76219.mp3');
+function playSound(soundFile) {
+	const audio = new Audio(soundFile);
 	audio.play();
 }
-function playSoundGreen() {
-	const audio = new Audio('C:100Dev Classes\\class12-materials\\class12-materials\\background-picker - Maria Midkiff\\cat-meow.wav');
-	audio.play();
-}
-function playSoundBlue() {
-	const audio = new Audio('C:100Dev Classes\\class12-materials\\class12-materials\\background-picker - Maria Midkiff\\canadian-geese.wav');
-	audio.play();
-}
-function playSoundOrange() {
-	const audio = new Audio('C:100Dev Classes\\class12-materials\\class12-materials\\background-picker - Maria Midkiff\\funk-trumpet_87bpm_G_minor.wav');
-	audio.play();
-}
+	
 function partyPurple() {
 	document.querySelector('body').style.backgroundColor = 'rgba(241, 63, 247, 1)'
 	document.querySelector('body').style.color = 'red'
